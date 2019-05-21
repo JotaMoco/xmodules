bcf3aa804918cdf3619a0478796c1dd442e0e3c0
JotaMoco <45787043+JotaMoco@users.noreply.github.com>
2019-05-21 11:03:22 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='xmodules',
+      version='0.1',
+      description='XModules Distribution Module',
+      author='Your Name',
+      author_email='Your Email',
+      url='The URL project - github for example',
+      py_modules=['xmodules'],
+     )
---
+++ xmodules.py
@@ -0,0 +1,15 @@
+def pi100pe ():
+	x = input("Introduza um número: ")
+	y = input("Introduza ou o mesmo número ou um número diferente: ")
+	if x == y:
+		print("Paravéns, és vué fixe!!!")
+	else:
+		print("És um ganda vurro!!!")
+def errocrasso ():
+	a = input("Introduza um número: ")
+	b = input("Introduza ou um número divisor do anterior ou um que não seja divisor: ")
+	if a % b:
+		print("Mereces um viscoito!!")
+	else:
+		print("És um cócó!!")
+__version__ = '0.1'
